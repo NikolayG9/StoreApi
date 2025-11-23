98e29d1cc9da71dab0ce4f13d1fc1f2bd0a7f081
Kolya <nikolaygolovach9@gmail.com>
2025-11-23 22:45:19 +0300

    Implement Forgot and Reset Password

---
+++ Store.Application/DataTransferObjects/ResetPasswordDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Store.Application.DataTransferObjects
+{
+    public class ResetPasswordDto
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
--- Store.Application/Services/Interfaces/IMailService.cs
+++ Store.Application/Services/Interfaces/IMailService.cs
@@ -3,5 +3,6 @@
     public interface IMailService
     {
         Task SendEmailAsync(string recipient, string subject, string body, byte[] pdfData, CancellationToken cancellationToken);
+        Task SendResetPasswordEmailAsync(string email, string token, string clientUrl, CancellationToken cancellationToken);
     }
 }
